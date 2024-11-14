b4493d786d41d712cf0d3323d235f3924180a82c
rahulkumarhavit@gmail.com <allblogwork24@gmail.com>
2024-11-14 23:54:23 +0530

    user api

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import  express from "express";
 import "dotenv/config";
+import routes from "./routes/index.js"
 const app = express();
 
 
 const PORT = 3000;
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(routes)
 
 app.get("/",(req,res)=>{
     return res.send("hi everyone.")
